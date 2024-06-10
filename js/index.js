fd7c5dcbd8ddfae94bb585103e057863f78839cd
Abigail Asante <asantea331@gmail.com>
2024-06-10 09:42:26 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+
+const btn = document.getElementById('btn-btn')
+btn.onclick = function () {
+    alert('wait, life is getting better')
+}
+
+const rob = document.getElementById('btn-b')
+rob.onclick = function () {
+    alert('robotics are making life better')
+}
